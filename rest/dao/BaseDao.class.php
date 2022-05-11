467849e50b8f24b0d5388adc5413f05e9a795057
ganicamna <amna.ganic@stu.ibu.edu.ba>
2022-05-11 12:28:31 +0200

    may 11

--- rest/dao/BaseDao.class.php
+++ rest/dao/BaseDao.class.php
@@ -10,11 +10,12 @@ class BaseDao{
   */
   public function __construct($table_name){
     $this->table_name = $table_name;
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $schema = "photography";
-    $this->conn = new PDO("mysql:host=$servername;dbname=$schema", $username, $password);
+    $servername = Config::DB_HOST();
+    $username = Config::DB_USERNAME();
+    $password = Config::DB_PASSWORD();
+    $schema = Config::DB_SCHEME();
+    $port = Config::DB_PORT();
+    $this->conn = new PDO("mysql:host=$servername;dbname=$schema;port=$port", $username, $password);
     // set the PDO error mode to exception
     $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   }
