96422dfe0d93c4fb397b949d004479bcd00341e4
ganicamna <amna.ganic@stu.ibu.edu.ba>
2022-05-11 10:48:24 +0200

    may 11

--- rest/index.php
+++ rest/index.php
@@ -6,7 +6,9 @@ error_reporting(E_ALL);
 require_once __DIR__.'/../vendor/autoload.php';
 require_once __DIR__.'/services/NoteService.class.php';
 require_once __DIR__.'/services/UserService.class.php';
+require_once __DIR__.'/dao/UserDao.class.php';
 
+Flight::register('userDao', 'UserDao');
 Flight::register('userService', 'UserService');
 Flight::register('noteService', 'NoteService');
 
@@ -15,6 +17,29 @@ Flight::map('error', function(Exception $ex){
     Flight::json(['message' => $ex->getMessage()], 500);
 });
 
+// middleware method for login
+Flight::route('/*', function(){
+  //perform JWT decode
+  $path = Flight::request()->url;
+  if ($path == '/login') return TRUE; // exclude login route from middleware
+
+  $headers = getallheaders();
+  if (@!$headers['Authorization']){
+    Flight::json(["message" => "Authorization is missing"], 403);
+    return FALSE;
+  }else{
+    try {
+      $decoded = (array)JWT::decode($headers['Authorization'], new Key(Config::JWT_SECRET(), 'HS256'));
+      Flight::set('user', $decoded);
+      return TRUE;
+    } catch (\Exception $e) {
+      Flight::json(["message" => "Authorization token is not valid"], 403);
+      return FALSE;
+    }
+  }
+});
+
+require_once __DIR__.'/routes/TodoRoutes.php';
 require_once __DIR__.'/routes/UserRoutes.php';
 require_once __DIR__.'/routes/NoteRoutes.php';
 
